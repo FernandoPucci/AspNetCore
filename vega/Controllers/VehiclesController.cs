d4f5620d10e1bf6d38713c32dd3076c5be143320
FernandoPucci <fsilvapucci@gmail.com>
2017-08-25 20:44:02 -0300

    Add Get Vehicle Verb

--- vega/Controllers/VehiclesController.cs
+++ vega/Controllers/VehiclesController.cs
@@ -113,5 +113,20 @@ namespace vega.Controllers
 
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id){
+
+            var vehicle = await context.Vehicles.Include(v=>v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle); 
+            
+            return Ok(vehicleResource);
+
+        }
     }
 }
