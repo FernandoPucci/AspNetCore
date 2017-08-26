b7b04a0b5b0afef2eccc2b7dcdb72b7eaf8df188
FernandoPucci <fsilvapucci@gmail.com>
2017-08-25 23:11:25 -0300

    Refactoring MappingProfile

--- vega/Mapping/MappingProfile.cs
+++ vega/Mapping/MappingProfile.cs
@@ -16,12 +16,12 @@ namespace vega.Mapping
             CreateMap<Model, KeyValuePairResource>();
             CreateMap<Feature, KeyValuePairResource>();
             CreateMap<Vehicle, SaveVehicleResource>()
-                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
-                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
+                .ForMember(svr => svr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
+                .ForMember(svr => svr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
             CreateMap<Vehicle, VehicleResource>()
                 .ForMember(vr => vr.Make, opt => opt.MapFrom(v => v.Model.Make))
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
-                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new KeyValuePairResource{Id = vf.Feature.Id, Name = vf.Feature.Name})));
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new KeyValuePairResource { Id = vf.Feature.Id, Name = vf.Feature.Name })));
 
             //API Resources to Domain
             CreateMap<SaveVehicleResource, Vehicle>()
@@ -30,10 +30,10 @@ namespace vega.Mapping
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
                 .ForMember(v => v.Features, opt => opt.Ignore())
-                .AfterMap((vr, v) =>
+                .AfterMap((svr, v) =>
                 {
                     #region Remove unselected features
-                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId)).ToList();
+                    var removedFeatures = v.Features.Where(f => !svr.Features.Contains(f.FeatureId)).ToList();
 
                     foreach (var f in removedFeatures.ToList())
                     {
@@ -42,7 +42,7 @@ namespace vega.Mapping
                     #endregion
 
                     #region Add new Features
-                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id }).ToList();
+                    var addedFeatures = svr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id }).ToList();
 
                     foreach (var f in addedFeatures)
                     {
